b4168259a380948d9de0bc8a3a36c6b42afdc112
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-04-07 10:24:14 -0300

    feat: initial web configuration

--- packages/env/drizzle_env.ts
+++ packages/env/drizzle_env.ts
@@ -2,13 +2,13 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const drizzle_env = createEnv({
-    server: {
-        DATABASE_URL: z.string().min(1),
-        PROJECT_ID: z.string().min(1),
-    },
-    runtimeEnv: {
-        DATABASE_URL: process.env.DATABASE_URL,
-        PROJECT_ID: process.env.PROJECT_ID,
-    },
-    emptyStringAsUndefined: true,
+  server: {
+    DATABASE_URL: z.string().min(1),
+    PROJECT_ID: z.string().min(1),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+  },
+  emptyStringAsUndefined: true,
 });
