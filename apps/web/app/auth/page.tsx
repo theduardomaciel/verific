a38159a3c9f0bff5738a0946626e919f5ed3c096
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-04-11 16:25:11 -0300

    fix: added responsive layout to auth screen

--- apps/web/app/auth/page.tsx
+++ apps/web/app/auth/page.tsx
@@ -1,5 +1,6 @@
 // Icons
 import Logo from "@/public/logo.svg";
+import Ticket from "@/public/ticket.svg";
 
 // Components
 import GoogleButton from "@/components/auth/GoogleButton";
@@ -7,13 +8,15 @@ import GoogleButton from "@/components/auth/GoogleButton";
 export default function LoginPage() {
 	return (
 		<div className="flex flex-col md:flex-row min-h-screen">
-			<div className="md:flex md:w-1/2 md:m-8 rounded-b md:rounded bg-primary relative overflow-hidden p-12 flex-col justify-center md:justify-between bg-[linear-gradient(180deg,_#2563EB_0%,_#3B82F6_100%)]">
-				<h1 className="font-dashboard text-secondary font-extrabold text-6xl leading-16 tracking-normal">
+			<div className="flex md:w-1/2 md:m-8 rounded-b md:rounded bg-primary relative overflow-hidden p-6 md:p-12 flex-col justify-center md:justify-between bg-[linear-gradient(180deg,_#2563EB_0%,_#3B82F6_100%)] gap-4">
+				<h1 className="hidden md:flex font-dashboard text-secondary font-extrabold text-4xl md:text-5xl leading-[110%] tracking-normal">
 					Tecnologia de eventos ao seu alcance
 				</h1>
 
-				<div className="relative z-10 flex flex-col gap-6 text-white">
-					<Logo className="w-[139.828125px] h-[33.5390625px] gap-[11.39px]" />
+				<Ticket className="absolute -right-48 bottom-48 md:-bottom-36 text-white rotate-12" />
+
+				<div className="relative z-10 flex md:items-start flex-col max-md:py-6 gap-3 text-white">
+					<Logo className="h-10 md:h-8" />
 					<p className="hidden md:flex font-normal text-xs leading-none tracking-normal text-secondary opacity-80">
 						Copyright 2025 verifIC. Todos os direitos reservados
 					</p>
@@ -21,34 +24,36 @@ export default function LoginPage() {
 			</div>
 
 			{/* Right Part */}
-			<div className="w-full md:w-1/2 flex items-center justify-center p-8">
-				<div className="max-w-md w-full">
-					<h3 className="font-dashboard foreground text-2xl font-bold mb-2 text-center">
-						Autenticação
-					</h3>
+			<div className="w-full flex-1 md:w-1/2 flex flex-col items-center justify-center p-8">
+				<div className="flex flex-col gap-6 items-center justify-center w-full max-w-sm">
+					<div className="flex flex-col items-center justify-center gap-2">
+						<h3 className="font-dashboard foreground text-2xl font-bold mb-2 text-center">
+							Autenticação
+						</h3>
 
-					<p className="text-muted-foreground text-sm mb-8 text-center">
-						Entre com seu e-mail institucional para
-						<br />
-						acessar a plataforma
-					</p>
+						<p className="text-muted-foreground text-sm text-center">
+							Entre com seu e-mail institucional para
+							<br />
+							acessar a plataforma
+						</p>
+					</div>
 
 					{/* Botão de login com Google */}
 					<GoogleButton />
 
-					<p className="text-muted-foreground text-sm mb-8 text-center">
+					<p className="text-muted-foreground text-sm text-center">
 						Ao continuar, você concorda com nossos
 						<br />
 						<a
 							href="/terms-of-service"
-							className="text-muted-foreground text-sm mb-8 underline"
+							className="text-muted-foreground text-sm hover:text-foreground hover:underline"
 						>
 							Termos de Serviço
 						</a>{" "}
 						e{" "}
 						<a
 							href="/privacy-policy"
-							className="text-muted-foreground text-sm mb-8 underline"
+							className="text-muted-foreground text-sm hover:text-foreground hover:underline"
 						>
 							Política de Privacidade
 						</a>
--- apps/web/components/auth/GoogleButton.tsx
+++ apps/web/components/auth/GoogleButton.tsx
@@ -1,5 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
-export default function GoogleButton() {
-	return <Button variant={"secondary"}>Continuar com Google</Button>;
+import GoogleLogo from "@/public/icons/google.svg";
+
+export default function GoogleButton({ className }: { className?: string }) {
+	return (
+		<Button
+			className={cn("w-full h-fit py-2.5", className)}
+			variant={"outline"}
+		>
+			<GoogleLogo className="mr-2" />
+			Continuar com Google
+		</Button>
+	);
 }
