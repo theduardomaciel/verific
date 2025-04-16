a7f7de69717c99f02e1e7ecdff4a6ff562fc80ff
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-04-16 10:39:06 -0300

    fix: removed random route that was existing

--- apps/web/app/(public)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ToDo } from "@/components/to-do";
-
-export default function Landing() {
-	return (
-		<main className="flex flex-col items-center justify-center h-screen">
-			<ToDo />
-		</main>
-	);
-}
