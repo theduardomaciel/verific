d2c64a117bbed83ad748aee1e59c8dce0dc9f57b
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-04-16 10:27:28 -0300

    fix: added webpack configuration with turbopack

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
 
 		return config;
 	},
+	turbopack: {
+		rules: {
+			"*.svg": {
+				loaders: ["@svgr/webpack"],
+				as: "*.js",
+			},
+		},
+	},
 	images: {
 		remotePatterns: [new URL("https://github.com/**")],
 	},
